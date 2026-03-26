0584e9f2a9e866ddc0fb08d37950ab7b93a60a71
bini-x <erblinhiseni@gmail.com>
2026-03-26 13:46:38 +0100

    first commit; routes;

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bini-x/snippetbox
+
+go 1.26.1
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	msg := fmt.Sprintf("Display a specific snippet with ID: %v...", id)
+
+	w.Write([]byte(msg))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create/", snippetCreate)
+
+	log.Print("Starting on port :4000")
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
